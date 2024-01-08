784e7b17d993f4d8a53cc1a5aa04875f451d5ed0
S.I.Shevchenko <sergey12491997@gmail.com>
2024-01-08 22:02:04 +0300

    add type hint in Settings

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ import os
 import logging
 import asyncio
 from functools import lru_cache
-from typing import final
+from typing import Self, final
 from pathlib import Path
 from types import NoneType
 
@@ -23,19 +23,19 @@ class Settings:
 		return cls.instance
 
 	def __init__(self) -> None:
-		self.BASE_DIR = Path(__file__).resolve().parent.parent
-		self.DEBUG = True
-		self.BOT_START = True
+		self.BASE_DIR: Path = Path(__file__).resolve().parent.parent
+		self.DEBUG: bool = True
+		self.BOT_START: bool = True
 		self._DISPATCHER: Dispatcher | None = None
-		self.APP_HOST = os.getenv("APP_HOST")
-		self.APP_PORT = os.getenv("APP_PORT")
-		self.SECRET_KEY = os.getenv("SECRET_KEY")
-		self.API_KEY = os.getenv("API_KEY")
-		self.BOT_URL = os.getenv("BOT_URL")
-		self.BOT_TOKEN = os.getenv("BOT_TOKEN")
-		self.REGISTER_KEY = os.getenv("REGISTER_KEY")
-		self.DB_URL = os.getenv("DB_URL")
-		self.ALEMBIC_DB_URL = os.getenv("ALEMBIC_DB_URL")
+		self.APP_HOST: str | None = os.getenv("APP_HOST")
+		self.APP_PORT: str | None = os.getenv("APP_PORT")
+		self.SECRET_KEY: str | None = os.getenv("SECRET_KEY")
+		self.API_KEY: str | None = os.getenv("API_KEY")
+		self.BOT_URL: str | None = os.getenv("BOT_URL")
+		self.BOT_TOKEN: str | None = os.getenv("BOT_TOKEN")
+		self.REGISTER_KEY: str | None = os.getenv("REGISTER_KEY")
+		self.DB_URL: str | None = os.getenv("DB_URL")
+		self.ALEMBIC_DB_URL: str | None = os.getenv("ALEMBIC_DB_URL")
 		self._SERVER: uvicorn.Server | None = None
 
 	def _get_server(self) -> uvicorn.Server:
